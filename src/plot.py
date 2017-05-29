30066f0e8c66a3ea17e56f0a847cf309ae585e2c
vkomodey <vkomodey@gmail.com>
2017-05-29 09:41:53 +0300

    Add info to the title

--- src/plot.py
+++ src/plot.py
@@ -39,3 +39,38 @@ plt.savefig('f1.png')
 
 # Comment to prevent pop-up
 plt.show()
+
+# for ipython notebook
+%matplotlib inline
+
+
+def mesh2triang(mesh):
+    xy = mesh.coordinates()
+    return tri.Triangulation(xy[:, 0], xy[:, 1], mesh.cells())
+
+def plot(obj):
+    plt.gca().set_aspect('equal')
+    if isinstance(obj, Function):
+        mesh = obj.function_space().mesh()
+        if (mesh.geometry().dim() != 2):
+            raise(AttributeError)
+        if obj.vector().size() == mesh.num_cells():
+            C = obj.vector().array()
+            plt.tripcolor(mesh2triang(mesh), C)
+        else:
+            C = obj.compute_vertex_values(mesh)
+            plt.tripcolor(mesh2triang(mesh), C, shading='gouraud')
+    elif isinstance(obj, Mesh):
+        if (obj.geometry().dim() != 2):
+            raise(AttributeError)
+        plt.triplot(mesh2triang(obj), color='k')
+
+# example
+mesh = UnitSquareMesh(10, 10)
+plt.figure()
+plot(mesh)
+plt.show()
+Q = FunctionSpace(mesh, "CG", 1)
+F = interpolate(Expression("x[0]"), Q)
+plot(F)
+plt.show()
